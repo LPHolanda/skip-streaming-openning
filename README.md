095e702bc0914a6e5a7b381e1f9270fd79c3b139
LPHolanda <leandropholanda@Outlook.com>
2023-07-08 01:06:31 -0300

    tela para seleção de streaming

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# skip-streaming-add
+# skip-streaming-openning
+## Esse script pula a abertura automaticamente dos principais streamings do mercado
+## Um loop recursivo simples usando o pyautogui para localizar o print do botão de pular abertura e o time para esperar alguns segundos até acionar a recursividade
+### Netflix
+### Amazon Prime
+### HBO Max
+### Disney+
+### Star+
---
+++ skip.py
@@ -0,0 +1,82 @@
+import pyautogui
+import time
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.core.window import Window
+
+count = 0
+run = True
+streaming = ''
+
+pyautogui.useImageNotFoundException()
+
+GUI = Builder.load_file("skip.kv")
+
+class Screen(GridLayout):
+    btn = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(Screen, self).__init__(**kwargs)
+        Window.size = (500,500)
+        self.cols = 1
+        self.startButton = Button(
+            text = 'START'
+        )
+        self.startButton.bind(on_press=self.autoplay)
+        self.add_widget(self.startButton)
+
+        self.netflixButton = Button(
+            text = 'netflix'
+        )
+        self.netflixButton.bind(on_press=self.streaming)
+        self.add_widget(self.netflixButton)
+
+        self.primeButton = Button(
+            text = 'primevideo'
+        )
+        self.primeButton.bind(on_press=self.streaming)
+        self.add_widget(self.primeButton)
+
+        self.hboButton = Button(
+            text = 'hbo'
+        )
+        self.hboButton.bind(on_press=self.streaming)
+        self.add_widget(self.hboButton)
+
+        self.disneystar = Button(
+            text = 'disneystar'
+        )
+        self.disneystar.bind(on_press=self.streaming)
+        self.add_widget(self.disneystar)
+
+
+    def streaming(self, instance):
+        global streaming
+        streaming = instance.text
+
+
+    def autoplay(self, instance):
+        global run
+        global streaming
+        while run:
+            global count 
+            count = count+1
+            print("autoplay", count)
+            try:
+                play = pyautogui.locateOnScreen('streaming/'+ streaming +'.png')
+                pyautogui.click(play)
+                time.sleep(5)
+                self.autoplay(self)
+            except pyautogui.ImageNotFoundException:
+                print('ImageNotFoundException: image not found')
+
+
+class MeuApp(App):
+    def build(self):
+        return Screen()
+    
+    
+MeuApp().run()
